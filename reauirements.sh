9fb840692513342e02980e2c94bdd14641167fa7
Markus <48181660+BackInBash@users.noreply.github.com>
2022-01-29 13:46:48 +0100

    Updated Ansible Version

--- reauirements.sh
+++ reauirements.sh
@@ -1,2 +1,11 @@
-apt install ansible
+#!/bin/bash
+
+#
+# Debian 11 Installer
+#
+
+echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu focal main" > /etc/apt/sources.list.d/ansible.list
+
+apt-get update
+apt-get install -y ansible
 ansible-galaxy collection install devsec.hardening
