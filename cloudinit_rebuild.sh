135d89616d1363fdb076d7fec870ccce192e4e21
Markus <48181660+BackInBash@users.noreply.github.com>
2022-01-30 14:01:11 +0100

    Rebuild Clouinit

---
+++ cloudinit_rebuild.sh
@@ -0,0 +1,6 @@
+
+$(cd Debian/ && cloud-localds cloud-init.img cloud-init.yml)
+
+$(cd Ubuntu/ && cloud-localds cloud-init.img cloud-init.yml)
+
+$(cd Almalinux/ && cloud-localds cloud-init.img cloud-init.yml)
