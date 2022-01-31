2fd8e24ee1de45f748a2f03e68ae8a1be42edcfa
Markus <48181660+BackInBash@users.noreply.github.com>
2022-01-31 21:16:08 +0100

    Updated Ubuntu User Removal

--- Ubuntu/post_setup.sh
+++ Ubuntu/post_setup.sh
@@ -4,7 +4,10 @@
 apt-get autoclean
 apt-get autoremove -y
 
-# Remove sudoers
-sed -i -e 's/ubuntu    ALL=(ALL) NOPASSWD: ALL//g' /etc/sudoers
+# Remove ubuntu user
+sed -i -e 's/ubuntu ALL=(ALL) NOPASSWD: ALL//g' /etc/sudoers
 rm -rf /home/ubuntu
+passwd -d ubuntu
+usermod -s /usr/sbin/nologin ubuntu
+usermod -L ubuntu
 userdel -f ubuntu
